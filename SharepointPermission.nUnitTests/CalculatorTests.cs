f6329a3e4da654bbf925a4de0f891ac39aa2138b
Pham Quoc An <an.pham.quoc@preciofishbone.se>
2023-03-17 15:06:14 +0700

    Add testing unit to the project

---
+++ SharepointPermission.nUnitTests/CalculatorTests.cs
@@ -0,0 +1,35 @@
+using SharepointPermission.Services;
+
+namespace UnitTests;
+
+public class CalculatorTests
+{
+    private const int FirstNumber = 10;
+    private const int SecondNumber = 20;
+
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [Test]
+    [TestCase(1, 2)]
+    [TestCase(5, 10)]
+    [TestCase(50000, 900000)]
+    [TestCase(20, -10)]
+    public void TestAddTwoNumbers(int firstNumber, int secondNumber)
+    {
+        Assert.That(TestingService.AddTwoNumbers(FirstNumber, SecondNumber), Is.EqualTo(FirstNumber + SecondNumber));
+    }
+
+    [Test]
+    [TestCase(1, 2)]
+    [TestCase(5, 10)]
+    [TestCase(50000, 900000)]
+    [TestCase(20, -10)]
+    public void TestMultiplyTwoNumbers(int firstNumber, int secondNumber)
+    {
+        Assert.That(TestingService.MultiplyTwoNumbers(FirstNumber, SecondNumber),
+            Is.EqualTo(FirstNumber * SecondNumber));
+    }
+}
---
+++ SharepointPermission/Services/TestingService.cs
@@ -0,0 +1,14 @@
+namespace SharepointPermission.Services;
+
+public class TestingService
+{
+    public static int AddTwoNumbers(int firstNumber, int secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+
+    public static long MultiplyTwoNumbers(int firstNumber, int secondNumber)
+    {
+        return firstNumber * secondNumber;
+    }
+}
