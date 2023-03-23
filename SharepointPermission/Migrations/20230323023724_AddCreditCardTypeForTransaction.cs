27427d5c50da477a26fa259bc5413443ec9f21b2
Pham Quoc An <an.pham.quoc@preciofishbone.se>
2023-03-23 09:46:34 +0700

    Generated new migration for database

---
+++ SharepointPermission/Migrations/20230323023724_AddCreditCardTypeForTransaction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharepointPermission.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreditCardTypeForTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreditCardType",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreditCardType",
+                table: "Transactions");
+        }
+    }
+}
